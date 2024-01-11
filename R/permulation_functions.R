dec4ae7daacc7b0a108a14d774f84e8757610cad
emk270 <emk270@login0b.htc.sam.pitt.edu>
2024-01-10 20:13:49 -0500

    make function to return continuous permulated traits

--- R/permulation_functions.R
+++ R/permulation_functions.R
@@ -1,4 +1,6 @@
 require('RERconverge')
+require('phylolm')
+require('geiger')
 
 #'Produce a permulated phenotype tree from the observed phenotype and phylogeny
 #' @param foregrounds a vector of foreground species names
@@ -19,31 +21,74 @@ getPermulatedPhenotypeTree=function(foregrounds, neutraltree, root_species){
   names(masterTree) = c("masterTree")
 
   #permulated_tree = simBinPhenoCC(masterTree, neutraltree, root_species, fg_vec, sisters_list=sisters_list, pathvec, plotTreeBool = F)
-  print("Running permulation version with relaxed foreground topology requirements")
-  permulated_tree = simBinPhenoCC_relaxFGtopo(masterTree, neutraltree, root_species, fg_vec, sisters_list=sisters_list, pathvec, plotTreeBool = F)
-
+  #print("Running permulation version with relaxed foreground topology requirements")
+  #permulated_tree = simBinPhenoCC_relaxFGtopo(masterTree, neutraltree, root_species, fg_vec, sisters_list=sisters_list, pathvec, plotTreeBool = F)
+  print("Running permulation version that returns continuous traits simulated on tree (Pagel's lambda)")
+  #Calculate lambda here instead of in permulation function so we only need to calculate it once 
+  lambda<-getLambda(neutraltree, foregrounds)
+  print(lambda)
+  permulated_tree = simBinPhenoCC_continuousPagel(masterTree, root_species, lam=lambda, plotTreeBool=T)
   permulated_tree
 }
+
+#'Produce a vector of permulated continuous phenotypes from the observed binary phenotype and phylogeny
+#' @param foregrounds a vector of foreground species names
+#' @param neutraltree a phylo object representing neutral evolution
+#' @param root_species the species to root the tree on
+#' @return permulated_traits a vector of simulated continuous phenotype values
+#' @export
+getPermulatedContinuousPhenotype=function(foregrounds, neutraltree, root_species){
+  masterTree = list()
+  masterTree[[1]] = neutraltree
+  names(masterTree) = c("masterTree")
+
+  print("Running permulation version that returns continuous traits simulated on tree (Pagel's lambda)")
+  #Calculate lambda here instead of in permulation function so we only need to calculate it once
+  lambda<-getLambda(neutraltree, foregrounds)
+  print(lambda)
+  permulated_traits = simBinPhenoCC_continuousPagel(masterTree, root_species, lam=lambda, plotTreeBool=F)
+  permulated_traits
+}
+
 
 #'Produce permulated phenotype trees from the observed phenotype and phylogeny
 #' @param foregrounds a vector of foreground species names
 #' @param neutraltree a phylo object representing neutral evolution
 #' @param num_perms number of permulations
 #' @param root_species the species to root the tree on
-#' @param output_mod flag for the format of the output ("names" outputs permulated foreground names, "tree" outputs permulated phenotype trees)
+#' @param output_mod flag for the format of the output ("names" outputs permulated foreground names, "tree" outputs permulated phenotype trees, "continuous" outputs permulated continuous trait values)
 #' @return a list object containing permulated phenotypes
 #' @export
 getPermulatedPhenotypes=function(foregrounds, neutraltree, num_perms, root_species, output_mod="names"){
   fg_reps = lapply(1:num_perms, return_object, x=foregrounds)
-  permulated_trees = lapply(fg_reps, getPermulatedPhenotypeTree, neutraltree=neutraltree, root_species=root_species)
-
-  if (output_mod == "trees"){
+  if (output_mod == "continuous"){
+    permulated_trees = lapply(fg_reps, getPermulatedContinuousPhenotype, neutraltree=neutraltree, root_species=root_species)
     return(permulated_trees)
-  } else if (output_mod == "names"){
-    permulated_foregrounds = lapply(permulated_trees, getForegroundsFromTree)
-    return(permulated_foregrounds)
+  } else{
+      permulated_trees = lapply(fg_reps, getPermulatedPhenotypeTree, neutraltree=neutraltree, root_species=root_species)
+
+      if (output_mod == "trees"){
+        return(permulated_trees) 
+      } else if (output_mod == "names"){
+        permulated_foregrounds = lapply(permulated_trees, getForegroundsFromTree)
+        return(permulated_foregrounds)
+      }
   }
 }
+
+#' Calculate Pagel's lambda for a binary trait using Geiger's fitDiscrete function
+#' @param tree a phylogeny
+#' @param foregrounds a vector of foreground species names
+#' @return a numerical value (Pagel's lambda)
+#' @export
+getLambda=function(tree, foregrounds){
+  #Encode foreground or background as a binary trait for each species in the tree
+  in_fg<-sapply(tree$tip.label, function(x) if(x %in% foregrounds) "fg" else "bg")
+  #model=ARD (all rates different); can change this to an equal rates model? I think ARD makes more sense for most traits
+  fitLambda<-fitDiscrete(tree, in_fg, model="ARD", transform="lambda")
+  l<-fitLambda$opt$lambda
+  l
+} 
 
 #' @keywords internal
 getPathvecfromForegroundTree=function(foreground_tree){
@@ -62,4 +107,3 @@ getPathvecfromForegroundTree=function(foreground_tree){
 return_object=function(x_idx, x){
   return(x)
 }
-
